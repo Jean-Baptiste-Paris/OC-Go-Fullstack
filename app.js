1e3ea1bc853efd0b7de70b4abbe9513088c52a52
Jean-Baptiste-Paris <ecrireajb@gmail.com>
2024-02-20 21:47:28 +0100

    Create controllers and routes for User Update Prettier settings Add .prettierrc to .gitignore

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const stuffRoutes = require("./routes/stuff")
-
-const Thing = require("./models/Thing")
+const userRoutes = require("./routes/user")
 
 const app = express()
 mongoose
-    .connect(
-        "mongodb+srv://bob:s0wtG65XcN0801Q5@cluster0.xjgeyuo.mongodb.net/?retryWrites=true&w=majority"
-    )
-    .then(() => console.log("Connexion à MongoDB réussie"))
-    .catch(() => console.log("Connexion à MongoDB échouée"))
+  .connect(
+    "mongodb+srv://bob:s0wtG65XcN0801Q5@cluster0.xjgeyuo.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("Connexion à MongoDB réussie"))
+  .catch(() => console.log("Connexion à MongoDB échouée"))
 
 app.use(express.json())
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*")
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-    )
-    res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-    )
-    next()
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  )
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  )
+  next()
 })
 
 app.use("/api/stuff", stuffRoutes)
+app.use("/api/user", userRoutes)
 
 module.exports = app
