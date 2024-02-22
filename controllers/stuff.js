077c068f4bda446aa7f3d95600375cb905e0b1ef
Jean-Baptiste-Paris <ecrireajb@gmail.com>
2024-02-22 01:00:21 +0100

    Update PUT stuff route

--- controllers/stuff.js
+++ controllers/stuff.js
@@ -1,36 +1,25 @@
 const Thing = require("../models/Thing")
 
 exports.createThing = (req, res, next) => {
-  try {
-    // Vérifiez si req.body.thing est défini et non vide
-    if (!req.body.thing) {
-      return res
-        .status(400)
-        .json({ error: 'Missing or empty "thing" field in the request body.' })
-    }
-    const thingObject = JSON.parse(req.body.thing)
-    delete thingObject._id
-    delete thingObject._userId
-    const thing = new Thing({
-      ...thingObject,
-      userId: req.auth.userId,
-      imageUrl: `${req.protocol}://${req.get("host")}/images/${
-        req.file.filename
-      }`,
-    })
+  const thingObject = JSON.parse(req.body.thing)
+  delete thingObject._id
+  delete thingObject._userId
+  const thing = new Thing({
+    ...thingObject,
+    userId: req.auth.userId,
+    imageUrl: `${req.protocol}://${req.get("host")}/images/${
+      req.file.filename
+    }`,
+  })
 
-    thing
-      .save()
-      .then(() => {
-        res.status(201).json({ message: "Objet enregistré !" })
-      })
-      .catch((error) => {
-        res.status(400).json({ error })
-      })
-  } catch (error) {
-    console.error(error)
-    res.status(400).json({ error: 'Invalid JSON format in the "thing" field.' })
-  }
+  thing
+    .save()
+    .then(() => {
+      res.status(201).json({ message: "Objet enregistré !" })
+    })
+    .catch((error) => {
+      res.status(400).json({ error })
+    })
 }
 
 exports.getOneThing = (req, res, next) => {
@@ -46,8 +35,29 @@ exports.getAllStuff = (req, res, next) => {
 }
 
 exports.modifyThing = (req, res, next) => {
-  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: "Objet modifié" }))
+  const thingObject = req.file
+    ? {
+        ...JSON.parse(req.body.thing),
+        imageUrl: `${req.protocol}://${req.get("host")}/images/${
+          req.file.filename
+        }`,
+      }
+    : { ...req.body }
+
+  delete thingObject._userId
+  Thing.findOne({ _id: req.params.id })
+    .then((thing) => {
+      if (thing.userId != req.auth.userId) {
+        res.status(401).json({ message: "Non autorisé" })
+      } else {
+        Thing.updateOne(
+          { _id: req.params.id },
+          { ...req.body, _id: req.params.id }
+        )
+          .then(() => res.status(200).json({ message: "Objet modifié" }))
+          .catch((error) => res.status(400).json({ error }))
+      }
+    })
     .catch((error) => res.status(400).json({ error }))
 }
 
--- routes/stuff.js
+++ routes/stuff.js
@@ -8,7 +8,7 @@ const stuffCtrl = require("../controllers/stuff")
 router.post("/", auth, multer, stuffCtrl.createThing)
 router.get("/:id", auth, stuffCtrl.getOneThing)
 router.get("/", auth, stuffCtrl.getAllStuff)
-router.put("/:id", auth, stuffCtrl.modifyThing)
+router.put("/:id", auth, multer, stuffCtrl.modifyThing)
 router.delete("/:id", auth, stuffCtrl.deleteThing)
 
 module.exports = router
